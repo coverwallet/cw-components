9fdfda9aa7199461267bc8b41c63d5d78710bc2b
Dmitry Kremez <kremez19@gmail.com>
2016-02-04 16:04:36 +0300

    Add Number Input

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import InputKeyboard from '../../lib/index';
+import ReactDOM from 'react-dom';
+import { InputKeyboard } from '../../lib/index';
+import { NumberInput } from '../../lib/index';
 
 let App = React.createClass({
   render() {
     return (
       <div className="example">
         <h1>cw-components</h1>
+        <h2>Keyboard input</h2>
         <InputKeyboard/>
+
+        <h2>Number input</h2>
+        <NumberInput />
       </div>
     );
   }
 });
 
-React.render(<App/>, document.getElementById('container'));
+ReactDOM.render(<App/>, document.getElementById('container'));
