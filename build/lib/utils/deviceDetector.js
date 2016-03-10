f27f42e9c640a2ba421e7aa645320f132dd501c0
Dmitry Kremez <kremez19@gmail.com>
2016-03-10 16:25:23 +0300

    Remove build dir, it will be installed in postinstall script

--- build/lib/utils/deviceDetector.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.isIOS = isIOS;
-function isIOS() {
-  return window && window.navigator && window.navigator.userAgent && window.navigator.userAgent.match(/(iPod|iPhone|iPad)/);
-}
