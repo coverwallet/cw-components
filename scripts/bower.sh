82344451b259f0121a7a8688cb6269dcb9bb1ca3
Dmitry Kremez <dzmitry.kremez@itechart-group.com>
2016-03-17 17:25:52 +0300

    Add bower webpack build

---
+++ scripts/bower.sh
@@ -0,0 +1,10 @@
+# Clean old build
+rm -rf build/
+
+# Transpile ES6
+babel -d build/lib ./lib
+
+# Generate bundle
+mkdir -p dist
+webpack --devtool source-map --config webpack.bower.js
+eval "MINIFY=1 ./node_modules/.bin/webpack --devtool source-map --config webpack.bower.js"
