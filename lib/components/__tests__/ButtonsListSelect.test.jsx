ff108ec95b5d50b118acec615fc47e2661999ac8
Sandra Oliveros <sandra.oliveros@coverwallet.com>
2018-11-07 11:32:20 +0100

    Fix tests to use its own data with the interface that the component needs

--- lib/components/__tests__/ButtonsListSelect.test.jsx
+++ lib/components/__tests__/ButtonsListSelect.test.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import insuranceTypes from './data/insuranceTypes.json';
 import { ButtonsListSelect, ButtonsListSelectOption } from '../..';
 
 describe('Buttons List Select', () => {
@@ -21,7 +20,7 @@ describe('Buttons List Select', () => {
       expect(options.length).toEqual(DEFAULT_PROPS.options.length);
     });
 
-    it('renders 6 options the first time by default', () => {
+    it('renders 2 options the first time by default', () => {
       const component = renderComponent();
       const options = getOptions(component);
 
@@ -38,7 +37,7 @@ describe('Buttons List Select', () => {
     });
 
     it(`renders as many options as itemsToShowFirstRender + itemsPerPage
-  the first time view more is clicked`, () => {
+the first time view more is clicked`, () => {
       const expectedResult = DEFAULT_OPTIONS_FIRST_PAGE + DEFAULT_OPTIONS_PER_PAGE;
 
       const component = renderComponent();
@@ -72,14 +71,14 @@ describe('Buttons List Select', () => {
     });
   });
 
-  describe('selected values', () => {
+  describe('selected options', () => {
     beforeEach(() => {
       DEFAULT_ON_SELECT_CALLBACK.mockReset();
       DEFAULT_ON_DESELECT_CALLBACK.mockReset();
     });
 
     it(`set selected prop of option to true when its value is included
-        in selectedValues`, () => {
+        in selectedOptions`, () => {
       const testProps = setProps({ selectedOptions: [DEFAULT_PROPS.options[0].value] });
       const component = renderComponent(testProps);
       const options = getOptions(component);
@@ -88,7 +87,7 @@ describe('Buttons List Select', () => {
     });
 
     it(`does NOT set selected prop of option to false when its value is NOT included
-        in selectedValues`, () => {
+        in selectedOptions`, () => {
       const testProps = setProps({ selectedOptions: [DEFAULT_PROPS.options[0].value] });
       const component = renderComponent(testProps);
       const options = getOptions(component);
@@ -123,24 +122,39 @@ describe('Buttons List Select', () => {
     });
   });
 
-  const DEFAULT_OPTIONS_FIRST_PAGE = 6;
-  const DEFAULT_OPTIONS_PER_PAGE = 12;
+  const DEFAULT_OPTIONS_FIRST_PAGE = 2;
+  const DEFAULT_OPTIONS_PER_PAGE = 3;
   const DEFAULT_ON_SELECT_CALLBACK = jest.fn();
   const DEFAULT_ON_DESELECT_CALLBACK = jest.fn();
+  const EXAMPLE_INSURANCE_TYPE = {
+    name: 'Business Owners Policy (BOP)',
+    label: 'Business Owners Policy (BOP)',
+    value: 'Business Owners Policy (BOP)',
+    slug: 'business-owners-policy-bop',
+    notes: 'notes',
+    what_is_it: ' what_is_it',
+    why_get_it: 'why_get_it',
+    tooltip_key: 'tooltip_key',
+    display_order: 1,
+    icon_name: 'business_owners_policy_bop',
+    iconClass: 'business_owners_policy_bop',
+    formatted_text: null,
+  };
+
+  const createOptionsExample = () => (
+    [0, 1, 2, 3, 4, 5, 6, 7].map(key => ({ ...EXAMPLE_INSURANCE_TYPE, value: EXAMPLE_INSURANCE_TYPE.value + key }))
+  );
+
+  const EXAMPLE_OPTIONS = createOptionsExample();
 
   const DEFAULT_PROPS = {
-    options: insuranceTypes.map((type) => Object.assign({},
-      type,
-      {
-        label: type.name,
-        value: type.name,
-        iconClass: type.icon_name,
-        infoText: type.what_is_it,
-      },
-    )),
+    options: EXAMPLE_OPTIONS,
     onSelect: DEFAULT_ON_SELECT_CALLBACK,
     onDeselect: DEFAULT_ON_DESELECT_CALLBACK,
+    itemsToShowFirstRender: DEFAULT_OPTIONS_FIRST_PAGE,
+    itemsPerPage: DEFAULT_OPTIONS_PER_PAGE,
   };
+
 
   const setProps = props => Object.assign({}, DEFAULT_PROPS, props);
   const PAGINATION_DISABLED_PROPS = Object.assign({}, DEFAULT_PROPS, { viewMoreEnabled: false });
